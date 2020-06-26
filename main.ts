999de156bcfa8ae42c7fb0c00dc1e5edbfad4c16
Jim Bennett <jimbobbennett@mac.com>
2020-06-26 16:07:41 -0700

    Fixing messages for multiple value types

--- main.ts
+++ main.ts
@@ -3,20 +3,19 @@ function Get_message_device_id (message: string) {
 }
 function Send_message (Type: string, value: number) {
     basic.showIcon(IconNames.Duck)
-    message_to_send = "" + device_id + ":" + Type + ":" + ("" + value)
+    message_to_send = "" + device_id + ":" + "" + ":" + ("" + value)
     radio.sendString(message_to_send)
     basic.clearScreen()
 }
-function Check_last_message_time (received_device_id: number) {
+function Check_last_message_time (received_device_id: number, received_value_type: string) {
     for (let received_message of received_messages) {
-        if (Get_message_device_id(received_message) == received_device_id) {
+        if ("this".includes("" + received_device_id + ":" + received_value_type + "=")) {
             message_received_time = Get_message_received_time(received_message)
             time_since_message = input.runningTime() - message_received_time
             if (time_since_message < 540000) {
                 return 0
             } else {
                 received_messages.removeAt(received_messages.indexOf(received_message))
-                return 1
             }
         }
     }
@@ -25,22 +24,26 @@ function Check_last_message_time (received_device_id: number) {
 radio.onReceivedString(function (receivedString) {
     basic.showIcon(IconNames.SmallDiamond)
     received_message_device_id = Get_message_device_id(receivedString)
+    received_message_value_type = Get_message_value_type(receivedString)
     if (device_id != received_message_device_id) {
-        if (Check_last_message_time(received_message_device_id) == 1) {
-            basic.showIcon(IconNames.Yes)
-            received_messages.push("" + received_message_device_id + ":" + input.runningTime())
+        if (Check_last_message_time(received_message_device_id, received_message_value_type) == 1) {
+            received_messages.push("" + received_message_device_id + ":" + received_message_value_type + "=" + input.runningTime())
             radio.sendString(receivedString)
+            basic.showIcon(IconNames.Yes)
         } else {
             basic.showIcon(IconNames.No)
         }
     } else {
         basic.showIcon(IconNames.No)
     }
-    basic.clearScreen()
 })
-function Get_message_received_time (message: string) {
-    return parseFloat(message.split(":")[1])
+function Get_message_value_type (message: string) {
+    return "this".split(":")[1]
 }
+function Get_message_received_time (message: string) {
+    return parseFloat(message.split("=")[1])
+}
+let received_message_value_type = ""
 let received_message_device_id = 0
 let time_since_message = 0
 let message_received_time = 0
